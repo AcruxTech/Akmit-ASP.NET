d9e5af082cefca54eca3d7f62464c7a4a4067aa5
AcruxTech <acrux_tech@mail.ru>
2022-04-17 13:22:26 +0300

    Initial commit

---
+++ Akmit/Shared/Automapper/MicroserviceProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Akmit.Shared.Automapper
+{
+    public class MicroserviceProfile : Profile
+    {
+        public MicroserviceProfile() {
+            /*CreateMap<UserRto, UserInformationBlo>()
+                .ForMember(x => x.Email, x => x.MapFrom(m => m.Email))
+                .ForMember(x => x.Login, x => x.MapFrom(m => m.Login));*/
+        }
+    }
+}
---
+++ Akmit/Startup.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Akmit
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddControllers();
+            services.AddDbContext<AkmitContext>(p =>
+                p.UseSqlite("Data Source=database.db; Foreign Keys=True"));
+            services.AddAutoMapper(typeof(MicroserviceProfile));
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.RequireHttpsMetadata = false;
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuer = true,                  
+                            ValidIssuer = AuthOptions.ISSUER,      
+                            ValidateAudience = true,                
+                            ValidAudience = AuthOptions.AUDIENCE, 
+                            ValidateLifetime = false,               
+                            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),  
+                            ValidateIssuerSigningKey = true,    
+                        };
+                    });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseCors(option => option.WithOrigins("http://localhost:8080", "https://akmit.ru")
+                                        .AllowAnyMethod()
+                                        .AllowAnyHeader()
+                                        .AllowCredentials());
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor |
+               ForwardedHeaders.XForwardedProto
+            });
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            using var scope = app.ApplicationServices.CreateScope();
+
+            var mapper = scope.ServiceProvider.GetRequiredService<IMapper>();
+            mapper.ConfigurationProvider.AssertConfigurationIsValid();
+
+            var dbContext = scope.ServiceProvider.GetRequiredService<AkmitContext>();
+            dbContext.Database.Migrate();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
